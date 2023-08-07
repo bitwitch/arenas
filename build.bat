178cf9f2f9e2d64ee821dbfaa030bbb6d06a1c2c
Shaw <shawwian@gmail.com>
2023-08-07 01:56:45 -0400

    growable arena leveraging virtual memory

--- build.bat
+++ build.bat
@@ -3,8 +3,10 @@ if not exist build\ mkdir build
 pushd build
 if "%1"=="fixed_capacity" (
 	cl /W4 /WX /Zi /std:c11 /fsanitize=address /nologo "%~dp0fixed_capacity.c"
-) else (
+) else if "%1"=="chaining" (
 	cl /W4 /WX /Zi /std:c11 /fsanitize=address /nologo "%~dp0chaining.c"
+) else (
+	cl /W4 /WX /Zi /std:c11 /fsanitize=address /nologo "%~dp0mmu.c"
 )
 popd
 
